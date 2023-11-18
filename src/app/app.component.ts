2c5032d4cd6bd218801ef321a816d9f02320c3f7
esarii <125924130+esarii@users.noreply.github.com>
2023-11-18 23:02:11 +0300

    task manager app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon'
+import { TaskListComponent } from './task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, MatIconModule, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-manager';
+  title = 'Task Manager';
+
+  isTaskListVisible: boolean = false;
+
+  openTaskList() {
+    this.isTaskListVisible = !this.isTaskListVisible;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
 
 export const routes: Routes = [];
