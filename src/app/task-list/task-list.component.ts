1e6d9d66250a735fc05ad3b28d5af3d1b23f10b8
esarii <125924130+esarii@users.noreply.github.com>
2023-11-19 02:25:23 +0300

    task manager list

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../task.model';
+import { TaskService } from '../task.service';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatInputModule, MatIconModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit{
+  tasks: Task[] = [];
+  newTaskTitle = '';
+
+  constructor(private taskService: TaskService) {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  ngOnInit() {
+
+  }
+
+  addTask() {
+    console.log("ezgi");
+    if (this.newTaskTitle.trim() !== '') {
+      const newTask: Task = {
+        id: this.tasks.length + 1,
+        title: this.newTaskTitle
+      };
+      this.taskService.addTask(newTask);
+      this.newTaskTitle = '';
+    }
+  }
+
+  removeTask(task: Task) {
+    this.taskService.removeTask(task);
+  }
 
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Task } from './task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+  private tasks: Task[] = [];
   constructor() { }
+
+  getTasks() {
+    return this.tasks;
+  }
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  removeTask(task: Task) {
+    const index = this.tasks.indexOf(task);
+    if (index > -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
 }
