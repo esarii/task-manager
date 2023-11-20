06f74a997b2ab6cb196bf8578c6aaad09b1ed83d
esarii <125924130+esarii@users.noreply.github.com>
2023-11-20 19:34:44 +0300

    snackbar checkbox getirildi

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -6,24 +6,38 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon'
+import {MatCheckboxModule} from '@angular/material/checkbox'
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatButtonModule, MatInputModule, MatIconModule],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatInputModule, MatIconModule, MatCheckboxModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent implements OnInit{
   tasks: Task[] = [];
   newTaskTitle = '';
+  content = 'This is a snackbar';
 
-  constructor(private taskService: TaskService) {
+  constructor(private taskService: TaskService,private snackBar: MatSnackBar) {
     this.tasks = this.taskService.getTasks();
   }
 
   ngOnInit() {
 
+  }
+
+  showSnackbar(message: string, panelClass: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 2000,
+      panelClass: [panelClass]
+    });
+  }
+
+  onButtonClick() {
+    this.showSnackbar('Task Completed.', 'custom-panel-class');
   }
 
   addTask() {
